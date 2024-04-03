0a1d779cd997886fefc1fd08a6ac6fd4aedf0e88
Alexander Lindskog <aleli930@student.liu.se>
2024-04-03 17:14:18 +0200

    Styr och gas bytt till 8-bitar och i2c funkar

--- TSEA56_styrmodul/TSEA56_styrmodul/main.c
+++ TSEA56_styrmodul/TSEA56_styrmodul/main.c
@@ -7,61 +7,92 @@
 
 #include <avr/io.h>
 #include <avr/interrupt.h>
+#include <stdio.h>
 
+const int ADDR = 25;
+
+// Slave receiver
+#
+#define TW_SR_SLA_ACK		0x60
+#define TW_SR_GEN_ACK		0x70
+#define TW_SR_DATA_ACK		0x80
+#define TW_SR_DATA_NOT_ACK	0x88
+
+void i2c_init()
+{
+	TWAR = (ADDR<<TWA0)|(0<<TWGCE);
+	TWCR = (1<<TWINT)|(1<<TWEA)|(0<<TWSTA)|(0<<TWSTO)|(1<<TWEN)|(1<<TWIE);  // Slave receiver mode
+}
+
+void pwm_init()
+{
+	// use fast PWM
+	
+	// Gasreglage
+	DDRD = 0xff; // output
+	
+	TCCR1A = (1<<COM1A0)|(2<<COM1B0)|(3<<WGM10);
+	TCCR1B = (3<<WGM12)|(2<<CS10);
+	
+	OCR1A = 20597; // OCR0A should be the PWM 
+	OCR1B = (1.5 * 2059.7) / 2; // OCR0B should be the pulse length 1.5 ms / 20 ms * TOP
+	
+	// Styrservo
+	DDRB = 0xff; // output
+	
+	TCCR3A = (1<<COM3A0)|(2<<COM3B0)|(3<<WGM30);  // Set when timer hits BOTTOM, clear on compare match, mode 3 fast PWM
+	TCCR3B = (3<<WGM32)|(2<<CS30);  // other part of WGM0, clock select f / 1024
+	
+	OCR3A = 20597; // OCR0A should be the PWM frequency
+	OCR3B = (1.5 * 2059.7) / 2; // OCR0B should be the pulse length 1.5 ms / 20 ms * TOP
+}
 
 int main(void)
 {
 	int i;
 	
+	cli();
+	
+	DDRA = 0xff;
+	
 	// TWI
-	TWBR |= 
-	TWCR |= (1<<TWEA)|(0<<TWSTA)|(0<<TWSTO)|(1<<TWEN);  // Slave receiver mode
-	
-	
-	// output
-	DDRB = 0xff;
+	i2c_init();	
 	
 	// PWM
-	// use fast PWM
-	
-	// Gasreglage
-	TCCR0A = (1<<COM0A0)|(2<<COM0B0)|(3<<WGM00);  // Set when timer hits BOTTOM, clear on compare match, mode 3 fast PWM
-    TCCR0B = (1<<WGM02)|(5<<CS00);  // other part of WGM0, clock select f / 1024
-	
-	
-	OCR0A = 155; // OCR0A should be the PWM frequency
-	OCR0B = (1.6 * 155) / 20; // OCR0B should be the pulse length 1.5 ms / 20 ms * TOP
-	// OC0A is the port 
-	
-	// Styrservo
-	TCCR2A = (1<<COM2A0)|(2<<COM2B0)|(3<<WGM20);  // Set when timer hits BOTTOM, clear on compare match, mode 3 fast PWM
-	TCCR2B = (1<<WGM22)|(5<<CS20);  // other part of WGM0, clock select f / 1024
-	
-	OCR2A = 155; // OCR0A should be the PWM frequency
-	OCR2B = (1.5 * 155) / 20; // OCR0B should be the pulse length 1.5 ms / 20 ms * TOP
-	
+	pwm_init();
+
 	// Test timer
-	TIMSK1 = (1<<OCIE1A);
+	//TIMSK1 = (1<<OCIE1A);
     
 	// Enable interrupt
-	// EICRA |= (1<<ISC10)|(1<<ISC00);
+	//EICRA |= (1<<ISC10)|(1<<ISC00);
 	
 	sei();
 	
 	while (1) 
     {
+		cli();
 		i++;
+		sei();
     }
 }
 
-ISR(INT0_vect)
+ISR(TWI_vect)
 {
-	OCR0B = (2 * 155) / 20;
-	OCR2B = (2 * 155) / 20;
-}
-
-ISR(INT1_vect)
-{
-	OCR0B = (1.5 * 155) / 20;
-	OCR2B = (1.5 * 155) / 20;
+	uint8_t TW_STATUS = TWSR & 0xf8;
+	
+	switch(TW_STATUS)
+	{
+		case TW_SR_SLA_ACK:
+			TWCR = (1<<TWIE)|(1<<TWINT)|(1<<TWEA)|(1<<TWEN);
+			break;
+		case TW_SR_DATA_ACK:
+			PORTA = (2059.7 + TWDR * 0.0314) / 2;
+			OCR1B = (2059.7 + TWDR * 0.0314) / 2;
+			TWCR = (1<<TWIE)|(1<<TWINT)|(1<<TWEA)|(1<<TWEN);
+			break;
+		default:
+			TWCR = (1<<TWIE)|(1<<TWINT)|(1<<TWEA)|(1<<TWEN);
+			break;
+	}
 }
