2fba2299a50197623d27f585364f17b53385402f
Alexander Lindskog <aleli930@student.liu.se>
2024-04-04 16:53:40 +0200

    Implementerat så att gas och styr ändras med i2c-protokoll

--- TSEA56_styrmodul/TSEA56_styrmodul/main.c
+++ TSEA56_styrmodul/TSEA56_styrmodul/main.c
@@ -17,6 +17,12 @@ const int ADDR = 25;
 #define TW_SR_GEN_ACK		0x70
 #define TW_SR_DATA_ACK		0x80
 #define TW_SR_DATA_NOT_ACK	0x88
+#define TW_SR_STOP			0xA0
+
+volatile uint8_t i2c_byte_cnt = 0;  // Tell wich byte in sequence
+volatile uint8_t control_switch = 0;  // Key to gasrelage or styr
+volatile uint16_t control_signal = 0;  // 
+
 
 void i2c_init()
 {
@@ -28,8 +34,8 @@ void pwm_init()
 {
 	// use fast PWM
 	
-	// Gasreglage
-	DDRD = 0xff; // output
+	// Styrservo
+	DDRD = (1<<4); // Output on OC1B
 	
 	TCCR1A = (1<<COM1A0)|(2<<COM1B0)|(3<<WGM10);
 	TCCR1B = (3<<WGM12)|(2<<CS10);
@@ -37,8 +43,8 @@ void pwm_init()
 	OCR1A = 20597; // OCR0A should be the PWM 
 	OCR1B = (1.5 * 2059.7) / 2; // OCR0B should be the pulse length 1.5 ms / 20 ms * TOP
 	
-	// Styrservo
-	DDRB = 0xff; // output
+	// Gasreglage
+	DDRB = (1<<7); // Output on OC3B
 	
 	TCCR3A = (1<<COM3A0)|(2<<COM3B0)|(3<<WGM30);  // Set when timer hits BOTTOM, clear on compare match, mode 3 fast PWM
 	TCCR3B = (3<<WGM32)|(2<<CS30);  // other part of WGM0, clock select f / 1024
@@ -61,12 +67,6 @@ int main(void)
 	// PWM
 	pwm_init();
 
-	// Test timer
-	//TIMSK1 = (1<<OCIE1A);
-    
-	// Enable interrupt
-	//EICRA |= (1<<ISC10)|(1<<ISC00);
-	
 	sei();
 	
 	while (1) 
@@ -79,16 +79,48 @@ int main(void)
 
 ISR(TWI_vect)
 {
-	uint8_t TW_STATUS = TWSR & 0xf8;
+	uint8_t TW_STATUS = TWSR & 0xf8;  // Extract I2C status
 	
 	switch(TW_STATUS)
 	{
 		case TW_SR_SLA_ACK:
-			TWCR = (1<<TWIE)|(1<<TWINT)|(1<<TWEA)|(1<<TWEN);
+			PORTA = 0;
+			TWCR = (1<<TWIE)|(1<<TWINT)|(1<<TWEA)|(1<<TWEN);  // Enable new interrupt
 			break;
 		case TW_SR_DATA_ACK:
-			PORTA = (2059.7 + TWDR * 0.0314) / 2;
-			OCR1B = (2059.7 + TWDR * 0.0314) / 2;
+			switch(i2c_byte_cnt)
+			{
+				case 0:
+					control_switch = TWDR;
+					break;
+				case 1:
+					control_signal |= (TWDR<<0);
+					break;
+				case 2:
+					control_signal |= (TWDR<<8);
+					break;
+				default:
+					break;
+			}
+			i2c_byte_cnt++;
+			TWCR = (1<<TWIE)|(1<<TWINT)|(1<<TWEA)|(1<<TWEN);
+			break;
+		case TW_SR_STOP:
+			switch(control_switch)
+			{
+				case 1:
+					OCR1B = (2059.7 + control_signal * 0.0314) / 2;
+					break;
+				case 2:
+					OCR3B = (2059.7 + control_signal * 0.0314) / 2;
+					break;
+				default:
+					break;
+			}
+			PORTA = (OCR1B>>8);
+			i2c_byte_cnt = 0;
+			control_switch = 0;
+			control_signal = 0;
 			TWCR = (1<<TWIE)|(1<<TWINT)|(1<<TWEA)|(1<<TWEN);
 			break;
 		default:
