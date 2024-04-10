db21a63e9b976e413955cbd1ebb1f5fae83be453
Erik Karlsson <erika871@student.liu.se>
2024-04-10 12:28:55 +0000

    Upload New File

---
+++ CirkleFit_c_code/CircleFitByPratt_initialize.c
@@ -0,0 +1,20 @@
+/*
+ * Academic License - for use in teaching, academic research, and meeting
+ * course requirements at degree granting institutions only.  Not for
+ * government, commercial, or other organizational use.
+ *
+ * CircleFitByPratt_initialize.c
+ *
+ * Code generation for function 'CircleFitByPratt_initialize'
+ *
+ */
+
+/* Include files */
+#include "CircleFitByPratt_initialize.h"
+
+/* Function Definitions */
+void CircleFitByPratt_initialize(void)
+{
+}
+
+/* End of code generation (CircleFitByPratt_initialize.c) */
