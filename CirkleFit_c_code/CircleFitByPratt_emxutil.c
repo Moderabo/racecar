d0d0f41eed4ea4e33725393fb98ab2343790da10
Erik Karlsson <erika871@student.liu.se>
2024-04-10 12:28:06 +0000

    Upload New File

---
+++ CirkleFit_c_code/CircleFitByPratt_emxutil.c
@@ -0,0 +1,46 @@
+/*
+ * Academic License - for use in teaching, academic research, and meeting
+ * course requirements at degree granting institutions only.  Not for
+ * government, commercial, or other organizational use.
+ *
+ * CircleFitByPratt_emxutil.c
+ *
+ * Code generation for function 'CircleFitByPratt_emxutil'
+ *
+ */
+
+/* Include files */
+#include "CircleFitByPratt_emxutil.h"
+#include "CircleFitByPratt_types.h"
+#include <stdlib.h>
+
+/* Function Definitions */
+void emxFree_real_T(emxArray_real_T **pEmxArray)
+{
+  if (*pEmxArray != (emxArray_real_T *)NULL) {
+    if (((*pEmxArray)->data != (double *)NULL) && (*pEmxArray)->canFreeData) {
+      free((*pEmxArray)->data);
+    }
+    free((*pEmxArray)->size);
+    free(*pEmxArray);
+    *pEmxArray = (emxArray_real_T *)NULL;
+  }
+}
+
+void emxInit_real_T(emxArray_real_T **pEmxArray, int numDimensions)
+{
+  emxArray_real_T *emxArray;
+  int i;
+  *pEmxArray = (emxArray_real_T *)malloc(sizeof(emxArray_real_T));
+  emxArray = *pEmxArray;
+  emxArray->data = (double *)NULL;
+  emxArray->numDimensions = numDimensions;
+  emxArray->size = (int *)malloc(sizeof(int) * (unsigned int)numDimensions);
+  emxArray->allocatedSize = 0;
+  emxArray->canFreeData = true;
+  for (i = 0; i < numDimensions; i++) {
+    emxArray->size[i] = 0;
+  }
+}
+
+/* End of code generation (CircleFitByPratt_emxutil.c) */
