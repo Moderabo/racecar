ae6e1cdbf7b6842495c340218c012d5d8e1719f7
Erik Karlsson <erika871@student.liu.se>
2024-04-10 11:21:23 +0000

    Upload New File

---
+++ bezier_c_kod/bezier_terminate.c
@@ -0,0 +1,20 @@
+/*
+ * Academic License - for use in teaching, academic research, and meeting
+ * course requirements at degree granting institutions only.  Not for
+ * government, commercial, or other organizational use.
+ *
+ * bezier_terminate.c
+ *
+ * Code generation for function 'bezier_terminate'
+ *
+ */
+
+/* Include files */
+#include "bezier_terminate.h"
+
+/* Function Definitions */
+void bezier_terminate(void)
+{
+}
+
+/* End of code generation (bezier_terminate.c) */
