3eb670e466474a518c8a1bab9a9ba3f7a1db46c7
Erik Karlsson <erika871@student.liu.se>
2024-04-10 10:17:39 +0000

    Upload New File

---
+++ bezier_c_kod/bezier.h
@@ -0,0 +1,32 @@
+/*
+ * Academic License - for use in teaching, academic research, and meeting
+ * course requirements at degree granting institutions only.  Not for
+ * government, commercial, or other organizational use.
+ *
+ * bezier.h
+ *
+ * Code generation for function 'bezier'
+ *
+ */
+
+#ifndef BEZIER_H
+#define BEZIER_H
+
+/* Include files */
+#include "rtwtypes.h"
+#include <stddef.h>
+#include <stdlib.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Function Declarations */
+extern void bezier(const double p[6], double l[1500]);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
+/* End of code generation (bezier.h) */
