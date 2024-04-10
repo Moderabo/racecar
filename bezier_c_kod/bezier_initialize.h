797ed8b7eff47c66ccc848dfb6908f4ae5570dff
Erik Karlsson <erika871@student.liu.se>
2024-04-10 11:21:06 +0000

    Upload New File

---
+++ bezier_c_kod/bezier_initialize.h
@@ -0,0 +1,32 @@
+/*
+ * Academic License - for use in teaching, academic research, and meeting
+ * course requirements at degree granting institutions only.  Not for
+ * government, commercial, or other organizational use.
+ *
+ * bezier_initialize.h
+ *
+ * Code generation for function 'bezier_initialize'
+ *
+ */
+
+#ifndef BEZIER_INITIALIZE_H
+#define BEZIER_INITIALIZE_H
+
+/* Include files */
+#include "rtwtypes.h"
+#include <stddef.h>
+#include <stdlib.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Function Declarations */
+extern void bezier_initialize(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
+/* End of code generation (bezier_initialize.h) */
