84d98a60f0b96dcbfc44cc64b517115321e43dc3
Erik Karlsson <erika871@student.liu.se>
2024-04-24 11:42:06 +0200

    småändringar nära grind

--- Kommunikationsmodul/Reglering/Planner.h
+++ Kommunikationsmodul/Reglering/Planner.h
@@ -26,7 +26,7 @@ public:
         s.row(3) << x_goal, y_goal;
 
         Eigen::RowVectorXf distance_vec(size);
-        for(float u = 1.f/size; u <=(1.f-1.f/size); u = u + 1.f/size){
+        for(float u = 1.f/size; u <=(1.f-1.f/size); u = u + 1.f/(size-2)){
 
             float l1 = pow((1.f-u),(3));
             float l2 = 3*pow((1.f-u),(2))*(u);
@@ -42,44 +42,13 @@ public:
         l.row(size-1) << 0, 0, 0, 1; //special case because its 0 index and its hard to think..
 
         P = l * s; //Calculates the bezier curve
-        std::cout << P << std::endl;
 
         //Is done when initalizing..
         calc_ref = std::make_unique<Calc_ref>(P, x_goal, y_goal, goal_angle);
     } 
     virtual ~Planner() 
     {}
-    Eigen::MatrixXf update_P(int size=20)
-    {
-        int r = 0;
-        Eigen::MatrixXf l(size,4);
-        //Eigen::MatrixXf s(4,2);
-        s.row(0) << x_start, y_start;
-        s.row(1) << (x_start + 700*cos(start_angle)), (y_start + 700*sin(start_angle));
-        s.row(2) << (x_goal - 700*cos(goal_angle)), (y_goal - 700*sin(goal_angle));
-        s.row(3) << x_goal, y_goal;
-
-        Eigen::RowVectorXf distance_vec(size);
-
-        for(float u = 1.f/size; u <=(1.f-1.f/size); u = u + 1.f/(size-2)){
-
-            float l1 = pow((1.f-u),(3));
-            float l2 = 3*pow((1.f-u),(2))*(u);
-            float l3 = 3*(1.f-u)*pow((u),(2));
-            float l4 = pow((u),(3));
-
-            r = int(size*u);
-
-            l.row(r) << l1, l2, l3, l4;
-
-        }
-        l.row(0) << 1, 0, 0, 0;
-        l.row(size-1) << 0, 0, 0, 1;
-
-        P = l * s;
-
-        return P;
-    }
+    
     float getRefAngle(float car_x, float car_y, float car_angle)
     {
         return calc_ref->update_ref(size, car_x, car_y, car_angle);
