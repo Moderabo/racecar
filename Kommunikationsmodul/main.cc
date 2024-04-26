4ab83855160420e0858f7249be1869a89c56a0ca
Alexander Lindskog <alexanderlindskog@live.com>
2024-04-26 14:56:44 +0200

    State-hatering

--- Kommunikationsmodul/main.cc
+++ Kommunikationsmodul/main.cc
@@ -47,95 +47,102 @@ int main(int argc, const char * argv[])
 
     uint64_t t {0};
     uint64_t tstart {timestamp()};
-    int speed {0};
-
+    int state {0};
+    std::vector<float> commands;
 
     // fetch result and print it out
     for(int i = 0; i < 10000; ++i)
     {
-	if (ctrl_c_pressed){
+	    if (ctrl_c_pressed)
+        {
             break;
         }
 
-        // Fetch new LiDAR data
-        lidar.update();
-
-        //std::vector<Cluster> clusters {lidar.getPoints()};
-    	//saveClusters(clusters);
-
-        std::vector<Cone> cones {lidar.getCones()};
-	    //saveCones(cones);
-
-        // Find all valid gates
-        std::vector<AltGate> gates;
-        gates = findGates(cones);
-        //saveGates(gates);
-
-        // Find previous and next gate
-        std::pair<AltGate,AltGate> prev_next_gate;
-        prev_next_gate = findPrevNextGate(gates);
-
-        AltGate prev_gate { prev_next_gate.first };
-        AltGate next_gate { prev_next_gate.second };
-
-        /*std::cout << "Previous gate: " << prev_gate.x << ", "
-                                       << prev_gate.y << ", "
-                                       << prev_gate.angle << '\n';
-        std::cout << "Next gate: " << next_gate.x << ", "
-                                   << next_gate.y << ", "
-                                   << next_gate.angle << '\n';*/
-
-        // Route planning and calculation of
-        Planner bezier {prev_gate.x, prev_gate.y, prev_gate.angle,
-                        next_gate.x, next_gate.y ,next_gate.angle};
-
-        float angle_to_steer = bezier.getRefAngle(0, 0, 0);
-
-        //float angle_to_steer {3*atan2f(next_gate.y, next_gate.x)};
-        std::cout << "trying angle: " << angle_to_steer << std::endl;
-
-    	angle_to_steer = std::max(-1.f, std::min(angle_to_steer, 1.f));
-    	float gas = std::max(-1.f, std::min(1.2f*bezier.getRefSpeed(), 1.f));
-
-	mqtt_connection.pubCones(cones);
-        mqtt_connection.pubBezier(bezier.getBezier_points());
-        mqtt_connection.pubCurve(bezier.getBezier_curve());
-
-        /*i2c_connection.steer(angle_to_steer);
-    	sleep(0.1);
-    	i2c_connection.gas(0);
-    	sleep(0.1);
-        speed = i2c_connection.getSpeed();*/
-	std::vector<float> commands_value_list;
-
-	commands_value_list = mqtt_connection.receiveMsg();
-
-	if( commands_value_list.empty() ){
-		continue;
-	}
-
-	if(commands_value_list[2])
-	{
-		i2c_connection.steer(0);
-		sleep(0.01);
-           	i2c_connection.gas(0);
-		break;
-	}
-
-
-	i2c_connection.steer(commands_value_list[0]);
-	sleep(0.05);
-	i2c_connection.gas(commands_value_list[1]);
-	sleep(0.01);
-	speed = i2c_connection.getSpeed();
-
-
-
         t = timestamp() - tstart;
-	mqtt_connection.pubSpeed( t, speed );
+
+        // steer, gas, mode
+        commands = mqtt_connection.receiveMsg();
+
+        if( !commands.empty() )
+        {
+            state = static_cast<int>(commands.at(0));
+
+            switch ( state )
+            {
+                case 0:  // Idle
+                    lidar.stop();
+                    i2c_connection.steer(0);
+                    i2c_connection.gas(0);
+                    break;
+                case 1:  // Manual
+                    lidar.start();
+                    i2c_connection.steer(commands.at(1));
+                    i2c_connection.gas(commands.at(2));
+                    break;
+                case 2:  // Automatic
+                    lidar.start();
+                    break;
+                default:
+                    lidar.stop();
+                    i2c_connection.steer(0);
+                    i2c_connection.gas(0);
+                    break;
+            }
+        }
+
+        // Automatic & Manual: get lidar data
+        if ( state == 1 || state == 2 )
+        {
+            // Fetch new LiDAR data
+            lidar.update();
+
+            //std::vector<Cluster> clusters {lidar.getPoints()};
+            //saveClusters(clusters);
+
+            std::vector<Cone> cones {lidar.getCones()};
+            //saveCones(cones);
+
+            // Find all valid gates
+            std::vector<AltGate> gates;
+            gates = findGates(cones);
+            //saveGates(gates);
+
+            // Find previous and next gate
+            std::pair<AltGate,AltGate> prev_next_gate;
+            prev_next_gate = findPrevNextGate(gates);
+
+            AltGate prev_gate { prev_next_gate.first };
+            AltGate next_gate { prev_next_gate.second };
+
+            // Route planning and calculation of
+            Planner bezier {prev_gate.x, prev_gate.y, prev_gate.angle,
+                            next_gate.x, next_gate.y ,next_gate.angle};
+
+            mqtt_connection.pubCones(cones);
+            mqtt_connection.pubBezier(bezier.getBezier_points());
+            mqtt_connection.pubCurve(bezier.getBezier_curve());
+        }
+
+        // Automatic: steer and gas
+        if ( state == 2 )
+        {
+            float angle_to_steer = bezier.getRefAngle(0, 0, 0);
+            angle_to_steer = std::max(-1.f, std::min(angle_to_steer, 1.f));
+            float gas = std::max(-1.f, std::min(1.2f*bezier.getRefSpeed(), 1.f));
+
+            i2c_connection.steer(angle_to_steer);
+            sleep(0.05);
+            i2c_connection.gas(bezier.getRefSpeed);
+            sleep(0.01);
+        }
+
+        if ( state == 1 || state == 2 )
+        {
+            int speed = i2c_connection.getSpeed();
+            mqtt_connection.pubSpeed( t, speed );
+        }
 
     }
     return 0;
 }
-
 
